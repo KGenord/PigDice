629b54c9f3f754754cd809dd3272dc51685c9fa8
KGenord <kgenord@umich.edu>
2026-09-10 14:16:02 -0400

    Second attemp

--- PigDice.cpp
+++ PigDice.cpp
@@ -2,8 +2,7 @@
 #include <ctime>
 #include <cstdlib>
 
-
-// Build your solution starting from this code.
+// Build your solution starting from this code
 
 struct GameState {
     char choice;
@@ -19,37 +18,20 @@ void play_game(GameState &g);
 void take_turn(GameState &g);
 void roll(GameState &g);
 void hold(GameState &g);
-void turn_count(GameState &g)
 
-
-void addScore(GameState &gs) {
-    gs.game_score += 10;
-}
 
 int main() {
-    GameState my_game; // instantiate a GameState object
-    addScore(my_game);
-    std::cout << "Game score is: " << my_game.game_score << std::endl;
-   // display_rules(); // call the display_rules function
-    // play_game([&]my_game); // call the play_game function and pass the GameState object
+    GameState my_game; // instantiate a GameState object addScore(my_game);
+    //display_rules(); // call the display_rules function
+    play_game(my_game); // call the play_game function and pass the GameState object
     return 0;
 }
-void take_turn(GameState &game) {
-    g.turn_count++;
-    std::cout << "TURN" << mg.turn_count << " - Game Score: " <<:
-    while (!mg.turn_over) {
-        std::cout << "roll or hold?  r/h): " << std::endl;
-        std::cin >> g.choice;
-        if (mg.choice == 'r') {
-            roll([])
-        }
-    }
-}
 
 
-void play_game(GameState g) {
+
+void play_game(GameState &g) {
     while (!g.game_over) {
-        take_turn([&]g);
+        take_turn((g));
         g.game_score += g.score_this_turn;
         if (g.game_score >= 20) {
             g.game_over = true;
@@ -59,29 +41,48 @@ void play_game(GameState g) {
             g.score_this_turn = 0;
         }
     }
+    std::cout << "You finished with a final score of ";
+    std::cout << g.game_score;
+    std::cout << " in " << g.turn_count << " turns ";
+    std::cout << "\nThanks for playing PIG Dice!";
+}
+void take_turn(GameState &g) {
+    g.turn_count++;
+    std::cout << "TURN " << g.turn_count;
+    std::cout << " - Game Score: " << g.game_score;
+    while (!g.turn_over) {
+        std::cout << "roll or hold? (r/h): ";
+        std::cin >> g.choice;
+        if (g.choice == 'r') {
+            roll(g);
+        }
+        else if (g.choice == 'h') {
+            hold(g);
+        }
+        else {
+            std::cout << "Invalid Choice!";
+        }
+    }
+    std::cout << "Score Banked This Turn: " << g.score_this_turn;
 }
 
-std::cout << "You finsihed with a final score of ";
-std::cout << g.game_score;
-std::cout << " in " << g.turn_count << " turns "
-std::cout << "\nThanks for playing PIG Dice!";
 
 void roll(GameState &g) {
-    srand(seed:(NULL));
+    srand(time(NULL));
     int die = rand() % 6 + 1;
     std::cout << "Die: " << die;
     if (die == 1) {
-        std::cout << "\nTurn over. No score.";
+        std::cout << "\nTurn over. No Score.";
         g.score_this_turn = 0;
         g.turn_over = true;
     }
     else {
         g.score_this_turn+=die;
-        std::cout << "Running score this turn:    " <<g.score_this_turn
+        std::cout << " Running score this turn: " << g.score_this_turn;
     }
 }
 
+
 void hold(GameState &g) {
     g.turn_over = true;
-
 }
